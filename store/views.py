8529f0471337077727ba86939b2cefae809619d2
BihawaM <bihawam271@gmail.com>
2020-12-10 09:33:46 +0300

    add def cart

--- store/views.py
+++ store/views.py
@@ -15,14 +15,14 @@ def store(request):
 	return render(request, 'store/store.html', context)
 
 
-# def cart(request):
-# 	data = cartData(request)
-# 	cartItems = data['cartItems']
-# 	order = data['order']
-# 	items = data['items']
+def cart(request):
+	data = cartData(request)
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
 
-# 	context = {'items':items, 'order':order, 'cartItems':cartItems}
-# 	return render(request, 'store/cart.html', context)
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/cart.html', context)
 
 # def checkout(request):
 # 	data = cartData(request)
