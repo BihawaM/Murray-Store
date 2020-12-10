4e86aa8bb6609e816c4a40e6555daf039b28a64c
BihawaM <bihawam271@gmail.com>
2020-12-10 09:32:32 +0300

    add def store

--- store/views.py
+++ store/views.py
@@ -5,14 +5,14 @@ import datetime
 from .models import * 
 from .utils import cookieCart, cartData, guestOrder
 
-# def store(request):
-# 	data = cartData(request)
-# 	cartItems = data['cartItems']
-# 	order = data['order']
-# 	items = data['items']
-# 	products = Product.objects.all()
-# 	context = {'products':products, 'cartItems':cartItems}
-# 	return render(request, 'store/store.html', context)
+def store(request):
+	data = cartData(request)
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+	products = Product.objects.all()
+	context = {'products':products, 'cartItems':cartItems}
+	return render(request, 'store/store.html', context)
 
 
 # def cart(request):
