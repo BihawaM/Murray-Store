08a3ee66ed7c51afc7fbaa3723e1e9338d9b7e9c
BihawaM <bihawam271@gmail.com>
2020-12-10 09:45:50 +0300

    add def guestOrder

--- store/utils.py
+++ store/utils.py
@@ -1,5 +1,5 @@
-import json
-from .models import *
+# import json
+# from .models import *
 
 # def cookieCart(request):
 
@@ -55,30 +55,30 @@ from .models import *
 # 	return {'cartItems':cartItems ,'order':order, 'items':items}
 
 	
-# def guestOrder(request, data):
-# 	name = data['form']['name']
-# 	email = data['form']['email']
+def guestOrder(request, data):
+	name = data['form']['name']
+	email = data['form']['email']
 
-# 	cookieData = cookieCart(request)
-# 	items = cookieData['items']
+	cookieData = cookieCart(request)
+	items = cookieData['items']
 
-# 	customer, created = Customer.objects.get_or_create(
-# 			email=email,
-# 			)
-# 	customer.name = name
-# 	customer.save()
+	customer, created = Customer.objects.get_or_create(
+			email=email,
+			)
+	customer.name = name
+	customer.save()
 
-# 	order = Order.objects.create(
-# 		customer=customer,
-# 		complete=False,
-# 		)
+	order = Order.objects.create(
+		customer=customer,
+		complete=False,
+		)
 
-# 	for item in items:
-# 		product = Product.objects.get(id=item['id'])
-# 		orderItem = OrderItem.objects.create(
-# 			product=product,
-# 			order=order,
-# 			quantity=item['quantity'],
-# 		)
-# 	return customer, order
+	for item in items:
+		product = Product.objects.get(id=item['id'])
+		orderItem = OrderItem.objects.create(
+			product=product,
+			order=order,
+			quantity=item['quantity'],
+		)
+	return customer, order
 
