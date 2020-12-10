16c98e9ac69fe687e9e2b01d483176226187b6ce
BihawaM <bihawam271@gmail.com>
2020-12-10 09:02:07 +0300

    add import json

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# import json
+import json
 # import datetime
 # from .models import * 
 # from .utils import cookieCart, cartData, guestOrder
